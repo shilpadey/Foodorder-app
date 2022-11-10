57278c744793146610a320863fa0b98e73c46d2a
shilpadey <shilpadey735@gmail.com>
2022-11-10 16:44:09 +0530

    meal summary

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import mealsImage from '../../assests/meals.jpg';
+import MealsSummary from "../Meals/MealsSummary";
 import classes from './Header.module.css';
 import HeaderCartButton from "./HeaderCartButton";
 
@@ -11,10 +12,11 @@ const Header = () => {
            <header className={classes.header}>
              <h1>ReactMeals</h1>
              <HeaderCartButton />
-           </header> 
+           </header>
            <div className={classes['main-image']}>
              <img src={mealsImage} alt="A table of delicious meals!" />
            </div>
+           <MealsSummary /> 
         </React.Fragment>
         
     );
