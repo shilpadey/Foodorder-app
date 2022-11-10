40d221cfeae8fc6c6bcd501f3c1aee65bd30c031
shilpadey <shilpadey735@gmail.com>
2022-11-10 17:04:54 +0530

    modified summary css

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -3,7 +3,7 @@ import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
     return (
-        <div className={classes.summary}>
+        <section className={classes.summary}>
             <h2>Delicious Food,Delivered To You</h2>
             <p>
                 Choose your meal from our broad selection of available meals and enjoy
@@ -13,7 +13,7 @@ const MealsSummary = () => {
                 All our meals are cooked with high quality ingredients,just-in-time and 
                 of course by experienced chefs!
             </p>
-        </div>
+        </section>
     );
 };
 
