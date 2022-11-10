5a076e2502c094cca3235a26792e14b091005079
shilpadey <shilpadey735@gmail.com>
2022-11-10 12:49:45 +0530

    modified

--- src/components/Cart/Cart.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import classes from './Cart.module.css';
-
-const Cart = () => {
-   
-};
-
-export default Cart;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,22 @@
 import React from "react";
 
-
+import mealsImage from '../../assests/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
 
 const Header = () => {
     return (
-        <header className={classes.header}>
-            <h1>ReactMeals</h1>
-            <button>Your Cart</button>
-        </header>
+        <React.Fragment>
+           <header className={classes.header}>
+             <h1>ReactMeals</h1>
+             <HeaderCartButton />
+           </header> 
+           <div className={classes['main-image']}>
+             <img src={mealsImage} alt="A table of delicious meals!" />
+           </div>
+        </React.Fragment>
+        
     );
 };
 
